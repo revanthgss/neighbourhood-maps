1324c5d1721aa594377aa609e808f0a59761fa34
revanthgss <g.revanth64@gmail.com>
2018-11-04 22:24:26 +0530

    Add map

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  renderMap = () => {
+    window.initMap = this.initMap;
+    addScript(`https://maps.googleapis.com/maps/api/js?key=AIzaSyDr74hacqaOzOqckWb9TnmXYnk8AMyKCfk&callback=initMap`);
+  }
+
+  initMap = () => {
+    let map = new window.google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: {lat: -25.344, lng: 131.036}});
+  }
+
+  componentDidMount() {
+    this.renderMap();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="map"></div>
       </div>
     );
   }
+}
+
+function addScript(url) {
+  const script = document.getElementsByTagName('script')[0];
+  let newScript = document.createElement('script');
+  newScript.src = url;
+  newScript.async = true;
+  newScript.defer = true;
+  script.parentNode.insertBefore(newScript, script);
 }
 
 export default App;
