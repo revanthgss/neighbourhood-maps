0a2100509594203649f8a89f3f5edb7b09177e83
revanthgss <g.revanth64@gmail.com>
2018-11-07 22:42:06 +0530

    Add accesibilty

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ class App extends Component {
   }
 
   componentDidMount() {
+    //Get the venues
     fetch(`https://api.foursquare.com/v2/venues/explore?client_id=QJ2RPGL0L5MVUQCYHDIO0PXBIRFHDPHC5VFVYVOWYQU5D0B3&client_secret=WQHUROLOPO0ZF1ASLZG0ANTJGKHNIF5DWN3FT5CVHCDV1N0I
     &v=20180323&limit=10&ll=${this.state.center.lat},${this.state.center.lng}&query=coffee`)
     .then((response) => {
@@ -26,6 +27,7 @@ class App extends Component {
     .then((response) => {
       this.setState({locations: response.response.groups[0].items});
     })
+    //Initialize the map
     .then(this.initMap)
     .catch((err) => {
       this.setState({fetchError:true});
@@ -33,10 +35,12 @@ class App extends Component {
   }
 
   initMap = () => {
+    //Create Map
     let map = new window.google.maps.Map(
       document.getElementById('map'), {zoom: 17, center: this.state.center});
-    
+    //Create blank Infowindow
     let infoWindow = new window.google.maps.InfoWindow();
+    //Initialize markers
     this.state.locations.forEach((item) => {
       let marker = new window.google.maps.Marker({
         name: item.venue.name,
@@ -45,25 +49,34 @@ class App extends Component {
       });
       this.markers.push(marker);
       let contentString = `<p>${item.venue.name}</p><p>${item.venue.location.address}</p>`;
+      //Event listener to show infowindow when marker is clicked
       marker.addListener('click', () => {
         infoWindow.setContent(contentString);
         infoWindow.open(map,marker);
+        //Show animation for 1 second
+        marker.setAnimation(window.google.maps.Animation.BOUNCE);
+        setTimeout(() => {
+          marker.setAnimation(null);
+        },1000);
       });
     })
     this.setState({map});
   }
 
   clickMarker = (name) => {
+    //Trigger click event for the marker having name
     let marker = this.markers.filter((marker) => marker.name===name);
     new window.google.maps.event.trigger(marker[0], 'click');
   }
 
   componentDidUpdate() {
+    //UpdateMarkers when state changes
     this.updateMarkers();
   }
 
   updateMarkers = () => {
     let showingMarkers=this.markers;
+    //Detach markers from the maps
     showingMarkers.forEach((marker) => {
       marker.setMap(null);
     })
@@ -71,6 +84,7 @@ class App extends Component {
       const match = new RegExp(escapeRegExp(this.state.query), 'i');
       showingMarkers = showingMarkers.filter((marker) => match.test(marker.name));
     }
+    //Set map for the filtered markers
     showingMarkers.forEach((marker) => {
       marker.setMap(this.state.map);
     })
@@ -78,13 +92,20 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="search-container">
+      <main className="App">
+        <section className="search-container">
           <SearchBar query={this.state.query} onUpdateQuery={this.updateQuery}/>
-          <SearchResults query={this.state.query} locations={this.state.locations} onClickLocation={this.clickMarker}/>
-        </div>
-        <div id="map"></div>
-      </div>
+          {this.state.fetchError && (
+            <div className="search-results">We are unable to get the locations from FourSquare</div>
+          )}
+          {!this.state.fetchError && (
+            <SearchResults query={this.state.query} locations={this.state.locations} onClickLocation={this.clickMarker}/>
+          )}
+        </section>
+        <div id="map" role="application" aria-describedby="map-description"></div>
+        <div id="map-description" hidden>A map showing all the restaurants near NewYork</div>
+        <div role="contentinfo" id="footer">Powered by <a href="https://foursquare.com/" target="_blank" rel="noopener noreferrer">FourSquare</a></div>
+      </main>
     );
   }
 }
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 const SearchBar = (props) => (
     <div className="search-bar">
-        <input value={props.query} onChange={(e) => props.onUpdateQuery(e.target.value)}></input>
+        <input 
+            value={props.query} 
+            placeholder={`Filter Restaurants`}
+            aria-label="Filter the Restaurants"
+            onChange={(e) => props.onUpdateQuery(e.target.value)}
+        ></input>
     </div>
 )
 
